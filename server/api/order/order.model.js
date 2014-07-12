7a48b7efc49e53588012bb505e2715a781c02c12
Cosmin Parvulescu <Cosmin.Parvulescu@gmail.com>
2014-07-12 15:36:42 +0300

    Added 'Order' model and api controller / route.

---
+++ server/api/order/order.model.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var OrderSchema = new Schema({
+  restaurant: String,
+  date: { type: Date, default: Date.now }
+  orderStatus: { type: Schema.ObjectId, ref: 'OrderStatus' }
+  owner: { type: Schema.ObjectId, ref: 'User' }
+  orderlines: [{ type: Schema.ObjectId, ref: 'Orderline'}]
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
