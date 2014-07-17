47d4fec4b54a0081deb68266ff3d2dec184a776f
claudiu <olteanu.claudiu@ymail.com>
2014-07-17 20:44:25 +0300

    Update mongodb url for development mode

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,6 +5,6 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/orderly-dev'
+      uri: 'mongodb://heroku:n2oK-uQHAb-Jr98ySHzCkGwFdbBUmNeozNVlNIGDnpj9q8sTYAyRjOAVEKJdSV4Obu7UnETIu8PM1j3PDMxKcQ@kahana.mongohq.com:10074/app27457096'
   }
 };
